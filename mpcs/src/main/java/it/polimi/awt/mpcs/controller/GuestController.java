cac37798977893202ddc78e7407ba93160fbb2dd
Antolas <anto.lasalandra@gmail.com>
2015-05-25 11:39:45 +0200

    Ultimi controller

--- mpcs/src/main/java/it/polimi/awt/mpcs/controller/GuestController.java
+++ mpcs/src/main/java/it/polimi/awt/mpcs/controller/GuestController.java
@@ -2,4 +2,23 @@ package it.polimi.awt.mpcs.controller;
 
 public class GuestController {
 
+	public String homePage(){
+		return null;
+	}
+	
+	public String getRegistration(){
+		return null;
+	}
+	
+	public String submitRegistration(){
+		return null;
+	}
+
+	public String getLogin(){
+		return null;
+	}
+	
+	public String submitLogin(){
+		return null;
+	}
 }
--- mpcs/src/main/java/it/polimi/awt/mpcs/controller/UserController.java
+++ mpcs/src/main/java/it/polimi/awt/mpcs/controller/UserController.java
@@ -2,4 +2,23 @@ package it.polimi.awt.mpcs.controller;
 
 public class UserController {
 
+	public String userHomePage(){
+		return null;
+	}
+	
+	public String getUnRegistration(){
+		return null;
+	}
+	
+	public String submitUnRegistration(){
+		return null;
+	}
+	
+	public String logout(){
+		return null;
+	}
+	
+	public String submitLogin(){
+		return null;
+	}
 }
